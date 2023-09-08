e93587b73e6e2bf3c0c0ef38bdbb7ce28b0ef1ff
Seung-wa Akpan <victorakpan169@gmail.com>
2023-09-08 05:53:32 +0100

    Updated Commit

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/api", (req, res) => {
     const { slack_name, track } = req.query;
     const currentDate = new Date();
     const currentDay = currentDate.toLocaleDateString('en-US', { weekday: 'long' });
